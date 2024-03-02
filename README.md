b747f926d590a6fe74c624048e082725948c8336
Andras Nagy <andras1000@gmail.com>
2024-03-02 09:40:15 +0100

    improve readme

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Django AWS Infrastructure
 
-![Architecture diagram](django-aws-infra.webp "Architecture diagram").
+![Architecture diagram](django-aws-infra.webp "Architecture diagram")
+
+## Provisioning
+
+```
+terraform apply
+```
+
+## Load Testing
+
+On Mac:
+
+```
+brew install k6
+```
+Then run:
+
+```
+k6 run load_test.js
+```
--- load_test.js
+++ load_test.js
@@ -1,13 +1,15 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
+const url = __ENV.URL || 'http://prod-1420744051.eu-west-1.elb.amazonaws.com/health'; // Default URL if not provided in environment
+
 export let options = {
     vus: 1000,  // 1000 virtual users (concurrent connections)
     duration: '240s',  // Total duration of the test
 };
 
 export default function () {
-    let res = http.get('http://prod-1420744051.eu-west-1.elb.amazonaws.com/health/');
+    let res = http.get(url);
     check(res, {
         'status is 200': (r) => r.status === 200,
     });
